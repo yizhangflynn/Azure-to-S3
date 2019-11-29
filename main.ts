4a0132962883bf0e5d5cbd776a7a08074cec84bb
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-11-28 19:31:10 -0500

    removed webpack and bable, and setup azure access

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-import 'config';
+import { get } from 'config';
+import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob';
+
+const azureConfig = get<{ account: string, key: string }>('azure');
+const azureCredential = new StorageSharedKeyCredential(azureConfig.account, azureConfig.key);
+const azureClient = new BlobServiceClient(`https://${azureConfig.account}.blob.core.windows.net`, azureCredential);
+
+async function listAzureBlobsByContainer(name: string): Promise<void> {
+    const container = azureClient.getContainerClient(name);
+    const blobs = await container.listBlobsFlat();
+
+    for await (const blob of blobs) {
+        console.log(blob.name);
+    }
+}
+
+listAzureBlobsByContainer('attachments').catch(error => console.log(error));
